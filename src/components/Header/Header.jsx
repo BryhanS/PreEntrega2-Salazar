8d8110f76f62da7ccd7f62c4ee035f8f0053ebef
BryhanS <80595094+BryhanS@users.noreply.github.com>
2023-10-01 03:38:23 +0000

    header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import NavBar from "../NavBar/NavBar";
+
+const Header = () => {
+  return (
+    <header className="header-container sticky-top">
+      <NavBar />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,49 @@
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg">
+      <div className="container-fluid">
+        <a className="navbar-brand logo-box" href="../index.html">
+          <img className="logo-img" src="../assets/img/logo.png" alt="logo" />
+          <span>Santa Maria</span>
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="collapse navbar-collapse header-menu justify-content-end"
+          id="navbarNavDropdown"
+        >
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" aria-current="page" href="../index.html">
+                Inicio
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link">Nosotros</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link">Tienda</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link">Blog</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link">Contacto</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
